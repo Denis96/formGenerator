0be9c8ea86779bafac400240d87893b5e5481acc
Denis96 <m8.denis.pereira@gmail.com>
2019-02-24 23:57:33 +0100

    end

--- js/main.js
+++ js/main.js
@@ -16,6 +16,8 @@ $(document).ready(function(){
 		var addSubOption = null;
 		var delSubOption = null;
 
+	var exportForm = null;
+
 	var delParent = null;
 	var delGrandParent = null;
 
@@ -44,6 +46,8 @@ $(document).ready(function(){
 				addSubOption =	$(".addSubOption");
 				delSubOption =	$(".delSubOption");
 
+			exportForm =	$(".generateForm");
+
 			delParent =			$(".delParent");
 			delGrandParent =	$(".delGrandParent");
 
@@ -58,15 +62,17 @@ $(document).ready(function(){
 			hiddenNames.keypress(changeFocusFunction);
 			hiddenNames.focusout(changeNameShowTextFunction);
 
-			addTextQuestion.click(		function() {	generateQuestionFunction("text")	});
-			addPasswordQuestion.click(	function() {	generateQuestionFunction("password")	});
-			addRadioQuestion.click(		function() {	generateQuestionFunction("radio")	});
-			addCheckboxQuestion.click(	function() {	generateQuestionFunction("checkbox")	});
-				addQuestion.click(		function() {	generateQuestionFunction("others")	});
+			addTextQuestion.click(		function() {	generateQuestionFunction($(this), "text")	});
+			addPasswordQuestion.click(	function() {	generateQuestionFunction($(this), "password")	});
+			addRadioQuestion.click(		function() {	generateQuestionFunction($(this), "radio")	});
+			addCheckboxQuestion.click(	function() {	generateQuestionFunction($(this), "checkbox")	});
+				addQuestion.click(		function() {	generateQuestionFunction($(this), "others")	});
 
 			addOption.click(		addOptionFunction 		);
 				addSubOption.click(	addSubOptionFunction	);
 				delSubOption.click(	removeSubOptionFunction	);
+
+			exportForm.click(	function() {	prepareDownload($(this)) }	);
 
 			delParent.click(		removeParentFunction 	);
 			delGrandParent.click(	removeGrandParentFunction 	);
@@ -74,82 +80,84 @@ $(document).ready(function(){
 
 	function GenerateFormFunction() {
 		var formName = $(this).next().val();
-		formName == "" ? formName = "Form name" : "";
+		if ( formName == "" ) {
+			alert("Add form name.");
+		} else {
 
-		console.log($(this).next().val());
+			var form = "";
 
-		var form = "";
-
-		form += "<div class='divForm'>";
 			form += "<div class='divForm'>";
-				form += "<h2 class='formName'>Form Name: ";
-					form += "<span class='showName'>"+formName+"</span>";
-					form += "<input type='text' class='hiddenName' hidden='hidden'>";
-				form += "</h2>";
-				form += "<h3>";
-					form += "<span class='formName'>Action: ";
-						form += "<span class='showName'>google.es</span>";
+				form += "<div class='headerForm'>";
+					form += "<h2 class='formName'>Form Name: ";
+						form += "<span class='showName'>"+formName+"</span>";
 						form += "<input type='text' class='hiddenName' hidden='hidden'>";
-					form += "</span>";
-					form += " // ";
-					form += "<span class='formName'>Method: ";
-						form += "<select class='input uniquePos'>";
-							form += "<option value='get'>get</option>";
-							form += "<option value='post'>post</option>";
-						form += "</select>";
-					form += " // ";
-					form += "<span class='formName'>Method: ";
-						form += "<select class='input uniquePos'>";
-							form += "<option value='blank'>_blank</option>";
-							form += "<option value='self'>_self</option>";
-							form += "<option value='parent'>_parent</option>";
-							form += "<option value='top'>_top</option>";
-						form += "</select>";
-					form += "</span>";
-				form += "</h3>";
-			form += "</div>";
-			form += "<hr>";
-			form += "<div class='menuOptions'>";
-				form += "<p>";
-					form += "<span class='h2'>Options: </span>";
+					form += "</h2>";
+					form += "<h3>";
+						form += "<span class='formName'>Action: ";
+							form += "<span class='showName'>google.es</span>";
+							form += "<input type='text' class='hiddenName' hidden='hidden'>";
+						form += "</span>";
+						form += " // ";
+						form += "<span class='formName'>Method: ";
+							form += "<select class='input uniquePos'>";
+								form += "<option value='get'>get</option>";
+								form += "<option value='post'>post</option>";
+							form += "</select>";
+						form += " // ";
+						form += "<span class='formName'>Method: ";
+							form += "<select class='input uniquePos'>";
+								form += "<option value='blank'>_blank</option>";
+								form += "<option value='self'>_self</option>";
+								form += "<option value='parent'>_parent</option>";
+								form += "<option value='top'>_top</option>";
+							form += "</select>";
+						form += "</span>";
+					form += "</h3>";
+				form += "</div>";
+				form += "<hr>";
+					form += "<div class='form'>";
+					form += "</div>";
+				form += "<hr>";
+				form += "<div class='menuOptions'>";
+					form += "<p>";
+						form += "<span class='h2'>Options: </span>";
 
-					form += "<button class='addTextQuestion buttonColorAdd leftPos'>add text</button> ";
-					form += "<button class='addPasswordQuestion buttonColorAdd middlePos'>add password</button> ";
-					form += "<button class='addRadioQuestion buttonColorAdd middlePos'>add radio</button> ";
-					form += "<button class='addCheckboxQuestion buttonColorAdd rightPos'>add checkbox</button> ";
+						form += "<button class='addTextQuestion buttonColorAdd leftPos'>add text</button> ";
+						form += "<button class='addPasswordQuestion buttonColorAdd middlePos'>add password</button> ";
+						form += "<button class='addRadioQuestion buttonColorAdd middlePos'>add radio</button> ";
+						form += "<button class='addCheckboxQuestion buttonColorAdd rightPos'>add checkbox</button> ";
 
-					//////////////////////////// ADD "DEL BOTTON" PARENTS_UNTIL(".DIVFORM") ////////////////////////////////////
-
-					form += "<button id='addQuestion' class='leftPos'>add other type</button> ";
-					form += "<select class='input rightPos'>";
-						form += "<option value='date'>(H5) Date</option>";
-						form += "<option value='time'>(H5) Time</option>";
-						form += "<option value='datetime'>(H5) Datetime</option>";
-						form += "<option value='month'>(H5) Month</option>";
-						form += "<option value='week'>(H5) Week</option>";
-						form += "<option value='number'>(H5) Number</option>";
-						form += "<option value='range'>(H5) Range</option>";
-						form += "<option value='color'>(H5) Color</option>";
-						form += "<option value='email'>(H5) Email</option>";
-						form += "<option value='file'>(H5) File</option>";
-						form += "<option value='tel'>(H5) Tel</option>";
-						form += "<option value='url'>(H5) Url</option>";
-						form += "<option value='search'>(H5) Search</option>";
-					form += "</select>";
-				form += "</p>";
-				form += "<div class='middleContent'>";
-					form += "<input type='text' class='leftPos' id='jsonName' name='jsonName' placeholder='Filename'>";
-					form += "<button id='generateJSON' class='rightPos'>Export form</button>";
+/*						form += "<button id='addQuestion' class='leftPos'>add other type</button> ";
+						form += "<select class='input rightPos'>";
+							form += "<option value='date'>(H5) Date</option>";
+							form += "<option value='time'>(H5) Time</option>";
+							form += "<option value='datetime'>(H5) Datetime</option>";
+							form += "<option value='month'>(H5) Month</option>";
+							form += "<option value='week'>(H5) Week</option>";
+							form += "<option value='number'>(H5) Number</option>";
+							form += "<option value='range'>(H5) Range</option>";
+							form += "<option value='color'>(H5) Color</option>";
+							form += "<option value='email'>(H5) Email</option>";
+							form += "<option value='file'>(H5) File</option>";
+							form += "<option value='tel'>(H5) Tel</option>";
+							form += "<option value='url'>(H5) Url</option>";
+							form += "<option value='search'>(H5) Search</option>";
+						form += "</select>";*/
+					form += "</p>";
+					form += "<div class='middleContent'>";
+						form += "<input type='text' class='leftPos' id='jsonName' name='jsonName' placeholder='Filename'>";
+						form += "<button class='generateForm rightPos'>Export form</button>";
+					form += "</div>";
 				form += "</div>";
 			form += "</div>";
-		form += "</div>";
 
-		$("#formContainer > .main").append(form);
-		$("#formContainer").removeAttr("hidden");
-		updateEvents();
+			$("#formContainer > .main").append(form);
+			$("#formContainer").removeAttr("hidden");
+			updateEvents();
+		}
 	}
 
-	function generateQuestionFunction(typeQuestion) {
+	function generateQuestionFunction(thisButton, typeQuestion) {
 		//	Create "fieldset" element with id
 			var fieldset = document.createElement("fieldset");
 			fieldset.setAttribute("id", ("question"+questionsCounter) );
@@ -200,33 +208,40 @@ $(document).ready(function(){
 
 
 		//	Create a Add, Delete
+
+
+		var br = document.createElement("br");
+
+		$(fieldset).append(br);
+		if (typeQuestion == "checkbox" || typeQuestion == "radio" ) {
 			var buttonAdd = document.createElement("button");
 			buttonAdd.setAttribute("class", "addOption" );
 			var buttonAddContent = document.createTextNode("add");
 			$(buttonAdd).append(buttonAddContent);
 
-			var buttonDelete = document.createElement("button");
-			buttonDelete.setAttribute("class", "buttonColorDel" );
-			var buttonDeleteContent = document.createTextNode("delete question");
-			$(buttonDelete).append(buttonDeleteContent);
+			$(fieldset).append(buttonAdd);
+		}
 
-			/*var buttonSwap = document.createElement("button");
-			buttonSwap.setAttribute("class", "swapQuestion" );
-			var buttonSwapContent = document.createTextNode("swap");
-			$(buttonSwap).append(buttonSwapContent);*/
-		
-		var br = document.createElement("br");
-
-		$(fieldset).append(br);
-		$(fieldset).append(buttonAdd);
-		//$(fieldset).append(buttonSwap);
-
-		$(form).append(fieldset);
+		$(thisButton).parentsUntil(".main", ".divForm").children(".form").append(fieldset);
 
 		questionsCounter++;
 
 		updateEvents();
 
+	}
+
+	function generateTextFunction() {
+			var div = document.createElement("div");
+			div.setAttribute("class", "options" );
+			$(div).append( patternOptionRadioFunction(1, questionsCounter, "text") );
+			return div;
+	}
+
+	function generatePasswordFunction() {
+			var div = document.createElement("div");
+			div.setAttribute("class", "options" );
+			$(div).append( patternOptionRadioFunction(1, questionsCounter, "password") );
+			return div;
 	}
 
 	function generateRadioFunction() {
@@ -238,48 +253,68 @@ $(document).ready(function(){
 			return div;
 	}
 
+	function generateCheckboxFunction() {
+			var div = document.createElement("div");
+			div.setAttribute("class", "options" );
+			for (var i = 1 ; i <= 2 ; i++) {
+				$(div).append( patternOptionRadioFunction(i, questionsCounter, "checkbox") );
+			}
+			return div;
+	}
+
 	// Returns a div with 2 options
 	function patternOptionRadioFunction(number, currentQuestion, optionType, level) {
 		var div = document.createElement("div");
 
-		//	Delete button
-			var buttonDelete = document.createElement("button");
-			buttonDelete.setAttribute("class", "delParent buttonOption buttonColorDel" );
-			var buttonDeleteContent = document.createTextNode("X");
-			$(buttonDelete).append(buttonDeleteContent);
+			if ( optionType=="radio" || optionType=="checkbox" ) { ////////////////////////// cambiar cuando se cambie la generacion de preguntas
 
-			if ( level!=2 ) { ////////////////////////// cambiar cuando se cambie la generacion de preguntas
+			//	Delete button
+				var buttonDelete = document.createElement("button");
+				buttonDelete.setAttribute("class", "delParent buttonOption buttonColorDel" );
+				var buttonDeleteContent = document.createTextNode("X");
+				$(buttonDelete).append(buttonDeleteContent);
+
 				//	Add button
 					var buttonAdd = document.createElement("button");
 					buttonAdd.setAttribute("class", "addSubOption buttonOption buttonColorAdd" );
 					var buttonAddContent = document.createTextNode("+");
 					$(buttonAdd).append(buttonAddContent);
+
+				//	Input (radio or checkbox (+15))
+					var input = document.createElement("input");
+					input.setAttribute("type", optionType );
+					input.setAttribute("disabled", "disabled" );
+					input.setAttribute("id", ("opt"+currentQuestion+"-"+number) );
+					input.setAttribute("name", ("question"+currentQuestion) );
+
+				//	Label for input
+					var label = document.createElement("label");
+					label.setAttribute("class", "showName" );
+					label.setAttribute("for", ("opt"+currentQuestion+"-"+number) );
+					var labelContent = document.createTextNode(("New option"));
+					$(label).append(labelContent);
+
+					var hiddenName = document.createElement("input");
+					hiddenName.setAttribute("type", "text" );
+					hiddenName.setAttribute("hidden", "true" );
+					hiddenName.setAttribute("class", "hiddenName" );
+
+				$(div).append(buttonDelete);
+				$(div).append(buttonAdd);
+				$(div).append(input);
+				$(div).append(label);
+				$(div).append(hiddenName);
+
+			} else if ( optionType=="text" || optionType=="password" ) {
+
+				var input = document.createElement("input");
+				input.setAttribute("type", optionType );
+				input.setAttribute("id", ("opt"+currentQuestion+"-"+number) );
+				input.setAttribute("name", ("question"+currentQuestion) );
+				
+				$(div).append(input);
+
 			}
-
-		//	Input (radio or checkbox (+15))
-			var input = document.createElement("input");
-			input.setAttribute("type", optionType );
-			input.setAttribute("disabled", "disabled" );
-			input.setAttribute("id", ("opt"+currentQuestion+"-"+number) );
-			input.setAttribute("name", ("question"+currentQuestion) );
-
-		//	Label for input
-			var label = document.createElement("label");
-			label.setAttribute("class", "showName" );
-			label.setAttribute("for", ("opt"+currentQuestion+"-"+number) );
-			var labelContent = document.createTextNode(("New option"));
-			$(label).append(labelContent);
-
-			var hiddenName = document.createElement("input");
-			hiddenName.setAttribute("type", "text" );
-			hiddenName.setAttribute("hidden", "true" );
-			hiddenName.setAttribute("class", "hiddenName" );
-
-		$(div).append(buttonDelete);
-		$(div).append(buttonAdd);
-		$(div).append(input);
-		$(div).append(label);
-		$(div).append(hiddenName);
 
 		return div;
 	}
@@ -375,11 +410,17 @@ $(document).ready(function(){
 
 
 	//////	Functions for download 
-		function prepareDownload() {
+		function generateFormFunction(event) {
+			var formContent = "";
+
+				formContent = $(event).parentsUntil(".main", ".divForm").children(".form").html();
+
+			return formContent;
+		}
+		function prepareDownload(event) {
 			var jsonName = $("#jsonName").val();
-			var contentJson = generateJSONFunction();
-			console.log(contentJson);
-			jsonName!="" ? download(jsonName+".json", contentJson) : download("your_json.json", contentJson);
+			var contentJson = generateFormFunction(event);
+			jsonName!="" ? download(jsonName+".html", contentJson) : download("your_form.html", contentJson);
 		}
 		function download(filename, text) {
 			var element = document.createElement('a');
